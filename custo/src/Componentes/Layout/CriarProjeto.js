1fd2d762cb1ed423a4aa34cb7504b419c18f0982
FernandoKohn <contatofernandokohn@gmail.com>
2024-01-16 19:58:50 -0300

    Dia 3

--- custo/src/Componentes/Layout/CriarProjeto.js
+++ custo/src/Componentes/Layout/CriarProjeto.js
@@ -1,38 +1,50 @@
+import { useEffect, useState } from "react"
 import styles from "./CriarProjeto.module.css"
 
 
-function CriarProjeto(MudarEstado) {
+function CriarProjeto({ MudarEstado, }) {
 
-    const EnviarForm = (e) => {
-        e.preventDefault()
-        fetch("https://api.jsonbin.io/v3/b/65a58b47266cfc3fde7914b6", {
-            method: "GET",
+    const [projeto, setProjeto] = useState({})
+
+    function EnviarForm(event) {
+        event.preventDefault()
+        var data = new FormData(event.target)
+        var value = Object.fromEntries(data.entries())
+        setProjeto(value)
+
+        fetch("https://api.jsonbin.io/v3/b/65a6d271266cfc3fde79a01f?meta=false", {
+            method: "POST",
             headers: {
-                "X-Master-key":"$2a$10$P91Taemis/4lebG1fO4lHe04u6tJJEIKvKhBtACSuqvfKLZc8sdlm"
+                "X-MASTER-KEY": "$2a$10$P91Taemis/4lebG1fO4lHe04u6tJJEIKvKhBtACSuqvfKLZc8sdlm",
             },
+            body: JSON.stringify(projeto),
         })
-        .then(response => response.json())
-        .then(console.log(response))
-        .catch(error => console.error("Error Fetching data:", error))
+            .then(resp => resp.json())
+            .then(data => console.log(data))
+
     }
 
-    return (
-     <form className={styles.Formulario}  onSubmit={EnviarForm}>
-        <header className={styles.FormHeader}>
-            <h1>Novo Projeto</h1>
-            <button onClick={() => MudarEstado}>Voltar</button>
-        </header>
-        <label htmlFor="ProjNome">Nome do Projeto</label>
-        <input type="text" name="nome" id="ProjNome" maxLength="20"/>
-        <label htmlFor="ProjTipo">Tipo do Projeto</label>
-        <input type="text" name="tipo" id="ProjTipo" maxLength="20" />
-        <label htmlFor="ProjOrca">Orçamento Inicial</label>
-        <input type="number" name="orcamento" id="ProjOrca" />
-        <label htmlFor="ProjPrazo">Prazo de Entrega</label>
-        <input type="date" name="prazo" id="ProjPrazo"/>
-        <button type="submit">Cadastrar Projeto</button>
-     </form>   
-    )
+
+
+return (
+    <>
+
+        <form className={styles.Formulario} onSubmit={EnviarForm}>
+            <header className={styles.FormHeader}>
+                <h1>Novo Projeto</h1>
+            </header>
+            <label htmlFor="ProjNome">Nome do Projeto</label>
+            <input type="text" name="nome" id="ProjNome" maxLength="20" />
+            <label htmlFor="ProjTipo">Tipo do Projeto</label>
+            <input type="text" name="tipo" id="ProjTipo" maxLength="20" />
+            <label htmlFor="ProjOrca">Orçamento Inicial</label>
+            <input type="number" name="orcamento" id="ProjOrca" />
+            <label htmlFor="ProjPrazo">Prazo de Entrega</label>
+            <input type="date" name="prazo" id="ProjPrazo" />
+            <button type="submit">Cadastrar Projeto</button>
+        </form>
+    </>
+)
 }
 
 export default CriarProjeto

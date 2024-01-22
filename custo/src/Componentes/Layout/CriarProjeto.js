17695c83363d918c374745b13446e0afa88f91a1
FernandoKohn <contatofernandokohn@gmail.com>
2024-01-22 14:32:52 -0300

    Update CriarProjeto.js

--- custo/src/Componentes/Layout/CriarProjeto.js
+++ custo/src/Componentes/Layout/CriarProjeto.js
@@ -11,37 +11,39 @@ function CriarProjeto({ MudarEstado, }) {
         var data = new FormData(event.target)
         var value = Object.fromEntries(data.entries())
         setProjeto(value)
-        fetch("https://my-json-server.typicode.com/FernandoKohn/CustoDB/projetos", {
-                method: 'POST',
-                
-                body: "!aaaa",
-            })
+        fetch('https://my-json-server.typicode.com/FernandoKohn/CustoDB/projetos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(value),
+        })
             .then(resp => resp.json())
             .then(data => console.log(data))
             .catch(err => console.log(err))
-            
+
     }
 
-return (
-    <>
-        
-        <form className={styles.Formulario} onSubmit={EnviarForm}>
-        
-            <header className={styles.FormHeader}>
-                <h1>Criar Projeto</h1>
-            </header>
-            <label htmlFor="ProjNome">Nome do Projeto</label>
-            <input type="text" name="nome" id="ProjNome" maxLength="20" />
-            <label htmlFor="ProjTipo">Tipo do Projeto</label>
-            <input type="text" name="tipo" id="ProjTipo" maxLength="20" />
-            <label htmlFor="ProjOrca">Orçamento Inicial</label>
-            <input type="number" name="orcamento" id="ProjOrca" />
-            <label htmlFor="ProjPrazo">Prazo de Entrega</label>
-            <input type="date" name="prazo" id="ProjPrazo" />
-            <button type="submit">Cadastrar Projeto</button>
-        </form>
-    </>
-)
+    return (
+        <>
+
+            <form className={styles.Formulario} onSubmit={EnviarForm}>
+
+                <header className={styles.FormHeader}>
+                    <h1>Criar Projeto</h1>
+                </header>
+                <label htmlFor="ProjNome">Nome do Projeto</label>
+                <input type="text" name="nome" id="ProjNome" maxLength="20" />
+                <label htmlFor="ProjTipo">Tipo do Projeto</label>
+                <input type="text" name="tipo" id="ProjTipo" maxLength="20" />
+                <label htmlFor="ProjOrca">Orçamento Inicial</label>
+                <input type="number" name="orcamento" id="ProjOrca" />
+                <label htmlFor="ProjPrazo">Prazo de Entrega</label>
+                <input type="date" name="prazo" id="ProjPrazo" />
+                <button type="submit">Cadastrar Projeto</button>
+            </form>
+        </>
+    )
 }
 
 export default CriarProjeto

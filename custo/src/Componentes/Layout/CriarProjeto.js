c0432df366b09ae37d3f677de48a6e90b9ad7715
FernandoKohn <contatofernandokohn@gmail.com>
2024-01-24 14:19:10 -0300

    Update CriarProjeto.js

--- custo/src/Componentes/Layout/CriarProjeto.js
+++ custo/src/Componentes/Layout/CriarProjeto.js
@@ -11,17 +11,6 @@ function CriarProjeto({ MudarEstado, }) {
         var data = new FormData(event.target)
         var value = Object.fromEntries(data.entries())
         setProjeto(value)
-        fetch('https://my-json-server.typicode.com/FernandoKohn/CustoDB/projetos', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(value),
-        })
-            .then(resp => resp.json())
-            .then(data => console.log(data))
-            .catch(err => console.log(err))
-
     }
 
     return (
